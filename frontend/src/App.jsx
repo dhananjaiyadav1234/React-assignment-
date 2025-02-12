f3499acee4f4434c115dff560dcf1107289eebbc
Dhananjai <dhananjaiyadav2006@gmail.com>
2025-02-12 10:07:41 +0530

    done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './app.css'
+
+function App() {
+  return (
+    <>
+    <div className='container'>
+      <h1>Dhananjai Yadav</h1>
+      
+      <h3>Email : dhananjaiyadav2006@gmail.com</h3>
+      <h3> Phone : 7004341111</h3>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
